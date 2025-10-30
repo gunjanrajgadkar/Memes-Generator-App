a69954a38df55800a3e0c714f86693fe207f362c
LokeshR <lokeshrajgadkar13@gmail.com>
2025-10-30 13:37:03 +0530

    Update script.js added WWEMemes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let url = "https://meme-api.com/gimme/";
 
 let subreddits = ["raimimemes","marvelmemes","ProgrammerHumor","catmemes","wholesomememes","dogmemes",
     "me_irl","dankmemes","Animemes","funny", "memes", "wholesomememes"
-    ,"AdviceAnimals", "comedyheaven","MadeMeSmile","animememes","wholesomeanimemes"];
+    ,"AdviceAnimals", "comedyheaven","MadeMeSmile","animememes","wholesomeanimemes","WWEMemes"];
 
 let getMeme = () => {
     let randomSubreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
